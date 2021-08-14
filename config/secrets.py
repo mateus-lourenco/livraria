2b347263a6c7fddaaf44ea78e865dfa300c0b1c1
Mateus Lourenço <mateus.lourenco.95@gmail.com>
2021-08-13 23:53:44 -0300

    refatorando estrutura do projeto

--- config/secrets.py
+++
@@ -1,8 +0,0 @@
-import string
-import secrets
-
-def key():
-
-    alphabet = string.ascii_letters + string.digits
-    password = ''.join(secrets.choice(alphabet) for i in range(20))
-    return password
