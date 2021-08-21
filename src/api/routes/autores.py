eff5e53c6ac0eae2007b100b64032ccb22da8a3a
Mateus Lourenço <mateus.lourenco.95@gmail.com>
2021-08-20 22:28:05 -0300

    inserindo PUT, PATCH e DELETE em autor

--- src/api/routes/autores.py
+++ src/api/routes/autores.py
@@ -7,7 +7,7 @@ from api.utils.database import db
 
 autor_routes = Blueprint("autor_routes", __name__)
 
-@autor_routes.route('/', methods=['POST'])
+@autor_routes.route("/", methods=["POST"])
 def create():
     try:
         data = request.get_json()
@@ -20,16 +20,46 @@ def create():
         print(e)
         return response_with(resp.INVALID_INPUT_422)
     
-@autor_routes.route('/', methods=['GET'])
+@autor_routes.route("/", methods=["GET"])
 def list():
     fetched = Autor.query.all()
-    autor_schema = AutorSchema(many=True, only=['nome','id'])
+    autor_schema = AutorSchema(many=True, only=["nome","id"])
     autores = autor_schema.dump(fetched)
     return response_with(resp.SUCCESS_200, value={"autores":autores})
 
-@autor_routes.route('/<int:autor_id>', methods=['GET'])
+@autor_routes.route("/<int:autor_id>", methods=["GET"])
 def get(autor_id):
     fetched = Autor.query.get_or_404(autor_id)
     autor_schema = AutorSchema()
     autor = autor_schema.dump(fetched)
     return response_with(resp.SUCCESS_200, value={"autor":autor})
+
+@autor_routes.route("/<int:autor_id>", methods=["PUT"])
+def update(autor_id):
+    data = request.get_json()
+    get_autor = Autor.query.get_or_404(autor_id)
+    get_autor.nome = data["nome"]
+    db.session.add(get_autor)
+    db.session.commit()
+    autor_schema = AutorSchema()
+    autor = autor_schema.dump(get_autor)
+    return response_with(resp.SUCCESS_200, value={"autor":autor})
+
+@autor_routes.route("/<int:autor_id>", methods=["PATCH"])
+def modify(autor_id):
+    data = request.get_json()
+    get_autor = Autor.query.get_or_404(autor_id)
+    if data.get("nome"):
+        get_autor.nome = data["nome"]
+    db.session.add(get_autor)
+    db.session.commit()
+    autor_schema = AutorSchema()
+    autor = autor_schema.dump(get_autor)
+    return response_with(resp.SUCCESS_200, value={"autor": autor})
+
+@autor_routes.route('/<int:autor_id>', methods=["DELETE"])
+def remove(autor_id):
+    get_autor = Autor.query.get_or_404(autor_id)
+    db.session.delete(get_autor)
+    db.session.commit()
+    return response_with(resp.SUCCESS_204)
