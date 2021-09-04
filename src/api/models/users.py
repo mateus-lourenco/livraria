79d3d7f2ebfb0b6b5cd6d3b3b1184e28dbe46a78
Mateus Lourenço <mateus.lourenco.95@gmail.com>
2021-09-04 17:41:28 -0300

    criando verificação de token

--- src/api/models/users.py
+++ src/api/models/users.py
@@ -8,6 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    isVerified = db.Column(db.Boolean, nullable=False, default=False)
+    email = db.Column(db.String(120), unique = True, nullable =False)
     
     def create(self):
         db.session.add(self)
@@ -17,6 +19,10 @@ class User(db.Model):
     @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username = username).first()
+    
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email = email).first()
     
     @staticmethod
     def generate_hash(password):
@@ -33,3 +39,4 @@ class UserSchema(SQLAlchemyAutoSchema):
         
     id = fields.Number(dump_only=True)
     username = fields.String(required=True)
+    email = db.Column(db.String(120), unique = True, nullable=False)
--- src/api/routes/users.py
+++ src/api/routes/users.py
@@ -5,6 +5,7 @@ from api.utils import responses as resp
 from api.models.users import User, UserSchema
 from api.utils.database import db
 from flask_jwt_extended import create_access_token
+from api.utils.token import generate_verification_token, confirm_verification_token
 
 
 user_routes = Blueprint("user_routes", __name__)
@@ -26,9 +27,16 @@ def create():
 def auth():
     try:
         data = request.get_json()
-        current_user = User.find_by_username(data["username"])
+        if data.get("email"):
+            current_user = User.find_by_email(data["email"])
+        elif data.get("username"):
+            current_user = User.find_by_username(data["username"])
+            
         if not current_user:
             return response_with(resp.SERVER_ERROR_404)
+        if current_user and not current_user.isVerified:
+            return response_with(resp.SERVER_ERROR_404)
+            
         if User.verify_hash(data["password"], current_user.passowrd):
             access_token = create_access_token(identity=data["username"])
             return response_with(resp.SUCCESS_201, value={"message":f"Logged in as {current_user.username}", 
@@ -38,3 +46,21 @@ def auth():
     except Exception as e:
         print(e)
         return response_with(resp.INVALID_INPUT_422)
+    
+@user_routes.route("/confirm/<token>", methods=["GET"])
+def verify_email(token):
+    try:
+        email = confirm_verification_token(token)
+    except:
+        return response_with(resp.SERVER_ERROR_403)
+    
+    user = User.query.filter_by(email=email).first_or_404()
+    
+    if user.isVerified:
+        return response_with(resp.INVALID_INPUT_422)
+    else:
+        user.isVerified = True
+        db.session.add(user)
+        db.session.commit()
+        return response_with(resp.SUCCESS_200, value={"message":
+                "E-mail verified, you can proceed to login now."})
